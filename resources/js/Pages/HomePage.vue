0206cbb2bc6c1c912f02462fdb255592bad3bbcb
Anastasya <aamokeeva@yandex.ru>
2024-01-15 12:55:36 +0300

    Creates laravel startup project, including Inertia, Vue3, Tailwind

---
+++ resources/js/Pages/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Layout from "../Shared/Layout.vue";
+
+defineProps({
+    title: {
+        type: String
+    }
+});
+</script>
+
+<template>
+    <Layout>
+        <h1 class="text-2xl font-bold">Hello, {{ title }}!</h1>
+    </Layout>
+</template>
---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'App Title'
+    }
+});
+
+const pageDescription = props.title + ' description';
+</script>
+
+<template>
+    <Head>
+        <title>{{ title }}</title>
+        <meta name="description" :content="pageDescription">
+    </Head>
+
+    <div class="container">
+        <slot/>
+    </div>
+</template>
